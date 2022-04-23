50d33b1efd10251e050495bddd0ff51e1fe2c2cd
seunoyeniyi <iamseunoyeniyi@gmail.com>
2022-04-23 09:03:25 +0100

    commufg

--- lib/ui/shop/shop.dart
+++ lib/ui/shop/shop.dart
@@ -28,86 +28,101 @@ class _ShopBodyState extends State<ShopBody> {
 
   @override
   Widget build(BuildContext context) {
-    return SingleChildScrollView(
-      child: Column(
-        children: [
-          Material(
-            //header container
-            elevation: 1,
-            child: Container(
-              padding: const EdgeInsets.all(0),
-              width: double.infinity,
-              height: 50,
-              decoration: const BoxDecoration(
-                color: Colors.white,
-                border: Border(top: BorderSide(color: AppColors.f1)),
-              ),
-              child: Row(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                children: [
-                  Expanded(
-                    child: ButtonTheme(
-                      alignedDropdown: true,
-                      child: DropdownButton(
-                        isExpanded: true,
-                        value: sortDropdownValue,
-                        icon: const Icon(Icons.arrow_drop_down),
-                        items: sortItems.map((String items) {
-                          return DropdownMenuItem(
-                            value: items,
-                            child: Text(
-                              items,
-                              style: const TextStyle(
-                                fontWeight: FontWeight.bold,
-                              ),
+    return Column(
+      children: [
+        Material(
+          //HEADER FILTER
+          elevation: 1,
+          child: Container(
+            padding: const EdgeInsets.all(0),
+            width: double.infinity,
+            height: 50,
+            decoration: const BoxDecoration(
+              color: Colors.white,
+              border: Border(top: BorderSide(color: AppColors.f1)),
+            ),
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                Expanded(
+                  child: ButtonTheme(
+                    alignedDropdown: true,
+                    child: DropdownButton(
+                      isExpanded: true,
+                      value: sortDropdownValue,
+                      icon: const Icon(Icons.arrow_drop_down),
+                      items: sortItems.map((String items) {
+                        return DropdownMenuItem(
+                          value: items,
+                          child: Text(
+                            items,
+                            style: const TextStyle(
+                              fontWeight: FontWeight.bold,
                             ),
-                          );
-                        }).toList(),
-                        onChanged: (String? newValue) {
-                          setState(() {
-                            sortDropdownValue = newValue!;
-                          });
-                        },
-                        underline: const SizedBox(),
-                      ),
+                          ),
+                        );
+                      }).toList(),
+                      onChanged: (String? newValue) {
+                        setState(() {
+                          sortDropdownValue = newValue!;
+                        });
+                      },
+                      underline: const SizedBox(),
                     ),
                   ),
-                  SizedBox(
-                    width: 50,
-                    child: Material(
-                      color: Colors.white,
-                      child: IconButton(
-                        icon: const Icon(Icons.search),
+                ),
+                SizedBox(
+                  width: 50,
+                  child: Material(
+                    color: Colors.white,
+                    child: IconButton(
+                      icon: const Icon(Icons.search),
+                      onPressed: () {},
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  width: 50,
+                  child: Material(
+                    color: Colors.white,
+                    child: IconButton(
                         onPressed: () {},
-                      ),
-                    ),
+                        icon: const Icon(
+                          Icons.tune,
+                        )),
                   ),
-                  SizedBox(
-                    width: 50,
-                    child: Material(
-                      color: Colors.white,
-                      child: IconButton(
-                          onPressed: () {},
-                          icon: const Icon(
-                            Icons.tune,
-                          )),
-                    ),
-                  ),
-                ],
-              ),
+                ),
+              ],
             ),
           ),
-          //SHOP PRODUCTS
-          Container(
-            margin: const EdgeInsets.only(top: 5),
-            color: Colors.white,
-            child: const Center(
-              child: Text("Shop"),
-            ),
-          )
-        ],
-      ),
+        ),
+        //SHOP PRODUCTS
+        SingleChildScrollView(
+          physics: const BouncingScrollPhysics(
+            parent: AlwaysScrollableScrollPhysics(),
+          ),
+          scrollDirection: Axis.vertical,
+          child: Column(
+            children: [
+              Expanded(
+                child: GridView.count(
+                  // shrinkWrap: true,
+                  // physics: const NeverScrollableScrollPhysics(),
+                  crossAxisCount: 3,
+                  crossAxisSpacing: 4.0,
+                  mainAxisSpacing: 8.0,
+                  children: List.generate(2, (index) {
+                    return const Center(
+                      child: ProductCard(),
+                    );
+                  }),
+                ),
+              ),
+            ],
+          ),
+        )
+      ],
     );
   }
 
